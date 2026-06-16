0e43db881a352743cdd0b9f308ef0f07846cfcda
Cesaraitools <cesarstore365@gmail.com>
2026-06-16 13:52:36 +0300

    Filter Facebook iOS Sentry noise

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -4,6 +4,26 @@
 
 import * as Sentry from "@sentry/nextjs";
 
+function isFacebookIosWebKitBridgeNoise(event: Sentry.Event) {
+  const exceptionValues = event.exception?.values ?? [];
+
+  return exceptionValues.some((exception) => {
+    const value = exception.value ?? "";
+    const frames = exception.stacktrace?.frames ?? [];
+    const hasWebKitMessageHandlersError = value.includes(
+      "window.webkit.messageHandlers"
+    );
+    const hasFacebookBridgeFrame = frames.some((frame) => {
+      const filename = frame.filename ?? "";
+      const functionName = frame.function ?? "";
+
+      return filename.startsWith("app:///") || functionName === "sendDataToNative";
+    });
+
+    return hasWebKitMessageHandlersError && hasFacebookBridgeFrame;
+  });
+}
+
 Sentry.init({
   dsn: "https://c66fec97c01df290b8e7884f524c864d@o4511319727865856.ingest.de.sentry.io/4511319729766480",
 
@@ -26,6 +46,14 @@ Sentry.init({
   // Enable sending user PII (Personally Identifiable Information)
   // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
   sendDefaultPii: true,
+
+  beforeSend(event) {
+    if (isFacebookIosWebKitBridgeNoise(event)) {
+      return null;
+    }
+
+    return event;
+  },
 });
 
 export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
